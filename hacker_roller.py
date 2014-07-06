92e527175dfbfeb436a471e782c059a14f154825
eldritchScholar <scientia.est.potestas@gmail.com>
2014-07-06 13:16:13 -0500

    A Shadowrun dice roller. Hacker style.

---
+++ hacker_roller.py
@@ -0,0 +1,28 @@
+import random
+
+hits = 0
+fails = 0
+
+
+print "HACKER ROLLER v0.1"
+pool = int(raw_input('DICE POOL: '))
+print "ROLLING..."
+for die in range(0, pool):
+    die = random.randint(1, 6)
+    print die
+    if die in range(5, 7):
+        hits = hits+1
+    elif die == 1:
+        fails = fails+1
+
+print "HITS: "+str(hits)
+
+if fails >= pool/2:
+    if hits == 0:
+        print "CRITICAL GLITCH!"
+        print "ERROR ERROR CRIT GLITCH EQUALS VERY YES"
+        hits = hits/hits
+    else:
+        print "GLITCH!"
+        print "ALL YOUR GLITCH IS BELONG TO YOU"
+
